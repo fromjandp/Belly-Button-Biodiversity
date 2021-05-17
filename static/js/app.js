4d81ba5aaab8b43a8afe5db588b173828cde9d3e
Judith Samuels <fromjandp@aol.com>
2021-05-17 18:27:11 -0500

    Bar Chart with hovertxt

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@
 var select_tag = d3.select("#selDataset");
 
 // Import the data, from the json file, into the javascript code for use.
-d3.json("samples.json").then((inputFileData) => {
+d3.json("./static/data/samples.json").then((inputFileData) => {
   console.log("inputFileData: ")
   console.log(inputFileData)
 
@@ -28,41 +28,43 @@ d3.json("samples.json").then((inputFileData) => {
 
 // The function is triggered by an option change in the Dropdown box of "Test Subject ID No" in index.html
 // <select id="selDataset" onchange="optionChanged(this.value)">
+
 function optionChanged(selected_id) {
   console.log("selected_id=", selected_id);
-​
-  //   Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-  // * Use`sample_values` as the values for the bar chart.
-  // * Use`otu_ids` as the labels for the bar chart.
-  // * Use`otu_labels` as the hovertext for the chart.
-  d3.json("samples.json").then((data) => {
-​
+
+  d3.json("./static/data/samples.json").then((data) => {
+    //   Create a horizontal bar chart with a dropdown menu 
+    //   to display the top 10 OTUs found in that individual.
+    //   "sample_values" - values for the bar chart.
+    //   "otu_ids"       - values for the labels for the bar chart.
+    //   "otu_labels"    - values for the hovertext for the chart.
+
     var samples = data.samples;
     var results = samples.filter(sampleObj => sampleObj.id == selected_id);
-​
+
     console.log("samples: ");
     console.log(samples);
-​
+
     var result = results[0];
-​
+
     console.log("results: ");
     console.log(results);
-​
+
     console.log("result: ");
     console.log(result);
-​
+
     var otu_ids = result.otu_ids;
     var otu_labels = result.otu_labels;
     var sample_values = result.sample_values;
-​
+
     var y_label = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
-​
+
     console.log("y_label: ");
     console.log(y_label);
-​
-    console.log("sample_valuese: ");
+
+    console.log("sample_values: ");
     console.log(sample_values.slice(0, 10).reverse());
-​
+
     var bar_trace = {
       y: y_label,
       x: sample_values.slice(0, 10).reverse(),
@@ -70,9 +72,9 @@ function optionChanged(selected_id) {
       type: "bar",
       orientation: "h",
     };
-​
+
     var data = [bar_trace];
-​
+
     var bar_layout = {
       title: "Top 10 OTUs",
       margin: { t: 30, l: 150 }
@@ -80,5 +82,5 @@ function optionChanged(selected_id) {
 
     Plotly.newPlot("bar", data, bar_layout);
 
-;    
-​
+  }); 
+}; 
